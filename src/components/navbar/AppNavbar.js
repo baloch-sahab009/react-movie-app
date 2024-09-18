f2476ebd380a006f1f92298adb78abdd838b6244
baloch-sahab009 <johnywanda872@gmail.com>
2024-09-18 13:02:35 -0700

    navabe responsive

--- src/components/navbar/AppNavbar.js
+++ src/components/navbar/AppNavbar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Navbar.css"
 import { Link } from "react-router-dom"
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const AppNavbar = () => {
     return (
@@ -9,8 +10,8 @@ const AppNavbar = () => {
               <div className="headerLeft">
                   <Link to="/"><img className="header__icon img-fluid" alt="logo" src="/images/logo.png" /></Link>
                   <Link to="/" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                  <Link to="/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                  <Link to="/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                  <Link to="/top_rated" style={{textDecoration: "none"}}><span>TopRated</span></Link>
+                  <Link to="/upcoming" style={{textDecoration: "none"}}><span>UpComing</span></Link>
               </div>
           </div>
         </div>
